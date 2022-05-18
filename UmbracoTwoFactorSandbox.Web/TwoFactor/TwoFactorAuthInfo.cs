157d337fce46a63940880270d6298a44ed1062e3
Gareth Wright <garpunkal@gmail.com>
2022-05-18 09:38:45 +0100

    Add project files.

---
+++ UmbracoTwoFactorSandbox.Web/TwoFactor/TwoFactorAuthInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+using Google.Authenticator;
+
+namespace UmbracoTwoFactorSandbox.Web.TwoFactor
+{
+    [DataContract]
+        public class TwoFactorAuthInfo
+        {
+            [DataMember(Name = "qrCodeSetupImageUrl")]
+            public string QrCodeSetupImageUrl { get; set; }
+
+            [DataMember(Name = "secret")]
+            public string Secret { get; set; }
+        }
+    }
---
+++ UmbracoTwoFactorSandbox.Web/TwoFactor/UmbracoAppAuthenticatorComposer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Security;
+using Umbraco.Cms.Web.BackOffice.Security;
+using Umbraco.Extensions;
+
+namespace UmbracoTwoFactorSandbox.Web.TwoFactor
+{
+    public class UmbracoAppAuthenticatorComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            var identityBuilder = new BackOfficeIdentityBuilder(builder.Services);
+
+            identityBuilder.AddTwoFactorProvider<UmbracoUserAppAuthenticator>(UmbracoUserAppAuthenticator.Name);
+
+            builder.Services.Configure<TwoFactorLoginViewOptions>(UmbracoUserAppAuthenticator.Name, options =>
+            {
+                options.SetupViewPath = "..\\App_Plugins\\TwoFactorProviders\\twoFactorProviderGoogleAuthenticator.html";
+            });
+        }
+    }
+}
